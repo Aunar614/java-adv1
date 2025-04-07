b16a0b25a535ca490dbe94c1e43302bdd8de3df6
Aunar614 <developkim614@gmail.com>
2025-04-07 12:43:45 +0900

    synchronized 코드 블럭

--- src/thread/sync/BankAccountV2.java
+++ src/thread/sync/BankAccountV2.java
@@ -15,18 +15,18 @@ public class BankAccountV2 implements BankAccount{
     public synchronized boolean withdraw(int amount) {
         log("거래 시작 : " + getClass().getSimpleName());
 
-        // 잔고가 출금액보다 적으면, 진행하면 안됨
+        // == 임계 영역 시작 ==
         log("[검증 시작] 출금액 : " + amount + "원, 잔액 : " + balance + "원");
         if (balance < amount) {
             log("[검증 실패] 출금액 : " + amount + "원, 잔액 : " + balance + "원");
             return false;
         }
 
-        // 잔고가 출금액보다 많으면, 진행
         log("[검증 완료] 출금액 : " + amount + "원, 잔액 : " + balance + "원");
-        sleep(1000);        // 출금에 걸리는 시간으로 가정
+        sleep(1000);
         balance = balance - amount;
         log("[출금 완료] 출금액 : " + amount + "원, 잔액 : " + balance + "원");
+        // == 임계 영역 종료 ==
 
         log("거래 종료");
         return true;
